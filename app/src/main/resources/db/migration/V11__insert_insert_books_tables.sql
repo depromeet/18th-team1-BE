77d09248910e9e331fb5d4a1b88a0fc74719e576
OhYeHyun <ohyhohyl@gmail.com>
2026-05-03 20:37:49 +0900

    ✨ feat: flyway book, quote에 더미 데이터 생성

---
+++ app/src/main/resources/db/migration/V11__insert_insert_books_tables.sql
@@ -0,0 +1,23 @@
+INSERT INTO books (title, author, publisher, isbn13, aladin_link)
+VALUES
+    (
+        '데미안',
+        '헤르만 헤세',
+        '민음사',
+        '9788937460449',
+        'https://www.aladin.co.kr'
+    ),
+    (
+        '어린 왕자',
+        '앙투안 드 생텍쥐페리',
+        '열린책들',
+        '9788932917245',
+        'https://www.aladin.co.kr'
+    ),
+    (
+        '모순',
+        '양귀자',
+        '쓰다',
+        '9788998441012',
+        'https://www.aladin.co.kr'
+    );
