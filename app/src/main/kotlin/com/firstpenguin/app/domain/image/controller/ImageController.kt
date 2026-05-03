1a9725183bb4a8080813c775046d57fc3c35a577
Seonjun Kim <kimsunjun1221@gmail.com>
2026-05-03 13:14:54 +0900

    ✨ feat: Presigned URL 발급 요청/응답 DTO 추가 (#44)

---
+++ app/src/main/kotlin/com/firstpenguin/app/domain/image/controller/ImageController.kt
@@ -0,0 +1,4 @@
+package com.firstpenguin.app.domain.image.controller
+
+class ImageController {
+}
---
+++ app/src/main/kotlin/com/firstpenguin/app/domain/image/dto/PresignedUrlResponse.kt
@@ -0,0 +1,6 @@
+package com.firstpenguin.app.domain.image.dto
+
+data class PresignedUrlResponse(
+    val presignedUrl: String,
+    val publicUrl: String,
+)
