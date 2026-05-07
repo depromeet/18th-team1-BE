e64fa6279a5b48e88d48af8860eae8142ac5fb5a
Seonjun Kim <kimsunjun1221@gmail.com>
2026-05-08 00:14:18 +0900

    Revert "🐛 fix: flyway V20 emotion_intensity 타입 변환 전 기존 데이터 삭제"
    This reverts commit 948acecf63459b7d777bad162b7a741871deb5bb.

--- app/src/main/resources/db/migration/V20__alter_diaries_emotion_intensity_integer.sql
+++ app/src/main/resources/db/migration/V20__alter_diaries_emotion_intensity_integer.sql
@@ -1,7 +1,3 @@
-DELETE FROM image_owners WHERE owner_type = 'DIARY';
-DELETE FROM diary_tags;
-DELETE FROM diaries;
-
 ALTER TABLE diaries
     ALTER COLUMN emotion_intensity TYPE INTEGER
     USING emotion_intensity::INTEGER;
