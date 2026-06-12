c285c6dd3cf8efa5c798d740025ff0385ae1a81e
OhYeHyun <ohyhohyl@gmail.com>
2026-06-12 22:41:24 +0900

    ✨ feat: RecommendationInput 에 Tag 와 analysis 추가

--- app/src/main/kotlin/com/firstpenguin/app/domain/recommendation/model/RecommendationInput.kt
+++ app/src/main/kotlin/com/firstpenguin/app/domain/recommendation/model/RecommendationInput.kt
@@ -1,10 +1,13 @@
 package com.firstpenguin.app.domain.recommendation.model
 
+import com.firstpenguin.app.domain.emotion.model.Tag
+
 data class RecommendationInput(
     val userId: Long,
     val emotionRangeId: Long,
-    val emotionTagIds: List<Long>,
-    val needTagId: Long?,
+    val emotionTags: List<Tag>,
+    val needTag: Tag?,
     val feelingText: String?,
     val diaryText: String?,
+    val analysis: UserInputAnalysis?,
 )
